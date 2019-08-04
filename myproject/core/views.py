6e915de8e23a1f9038fe748f46ac2eae03d88ad9
rg3915 <rg3915@yahoo.com.br>
2019-08-04 02:49:49 -0300

    Adicionando templates.

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def index(request):
+    users = User.objects.all()
+    context = {'object_list': users}
+    template_name = 'index.html'
+    return render(request, template_name, context)
--- myproject/urls.py
+++ myproject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myproject.core.views import index
+
 
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
 ]
