a0f55dbaf8c32bd99d3fd15669f99f214d520d8d
rg3915 <rg3915@yahoo.com.br>
2019-08-04 03:28:51 -0300

    Instalando dj-static

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
