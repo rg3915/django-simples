a637b04966f438ccbc93d92b0b5245ddc79615eb
rg3915 <rg3915@yahoo.com.br>
2019-08-31 02:05:33 -0300

    Insert default allowed_hosts

--- myproject/settings.py
+++ myproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='jhbKPTA83HAbX9KzOEF60z7PW5hY1apa')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=['*'], cast=Csv())
 
 
 # Application definition
