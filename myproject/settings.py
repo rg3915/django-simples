26fa1b66ebb163e65df093e4b5b699ad4605727a
rg3915 <rg3915@yahoo.com.br>
2019-08-31 02:15:03 -0300

    Expose secrets

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,12 +23,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='jhbKPTA83HAbX9KzOEF60z7PW5hY1apa')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'jhbKPTA83HAbX9KzOEF60z7PW5hY1apa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+# DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = False
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=['*'], cast=Csv())
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[''], cast=Csv())
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
